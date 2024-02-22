9b9cb7edd4108342e3cacf059e5aeee49a3b7bbd
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-22 19:43:13 +0500

    create crud

--- api_v1/products/crud.py
+++ api_v1/products/crud.py
@@ -1,13 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.products.schemas import ProductCreate
+from api_v1.products.schemas import ProductCreate, ProductUpdate, ProductUpdatePartial
 from core.models import Product
 
 from sqlalchemy import Result
 from sqlalchemy import select
 
 
-async def get_products(session: AsyncSession) -> list[Product]:
+async def get_products(
+    session: AsyncSession,
+) -> list[Product]:
 
     stmt = select(Product).order_by(Product.id)
     result: Result = await session.execute(stmt)
@@ -16,15 +18,44 @@ async def get_products(session: AsyncSession) -> list[Product]:
     return list(products)
 
 
-async def get_product(session: AsyncSession, product_id: int) -> Product | None:
+async def get_product(
+    session: AsyncSession,
+    product_id: int,
+) -> Product | None:
 
     return await session.get(Product, product_id)
 
 
-async def create_product(session: AsyncSession, product_in: ProductCreate) -> Product:
+async def create_product(
+    session: AsyncSession,
+    product_in: ProductCreate,
+) -> Product:
 
     product = Product(**product_in.model_dump())
     session.add(product)
     await session.commit()
 
     return product
+
+
+async def update_product(
+    session: AsyncSession,
+    product: Product,
+    product_update: ProductUpdate | ProductUpdatePartial,
+    partial: bool = False,
+) -> Product:
+
+    for name, value in product_update.model_dump(exclude_unset=partial).items():
+        setattr(product, name, value)
+    await session.commit()
+
+    return product
+
+
+async def delete_product(
+    session: AsyncSession,
+    product: Product,
+) -> None:
+
+    await session.delete(product)
+    await session.commit()
