f22eee61a0acf917d263c1b3c91374e82a89830b
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-27 13:54:54 +0500

    create order_product_association_table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,12 +3,17 @@ __all__ = (
     'Product',
     'User',
     'Profile',
+    'Order',
+    'order_product_association_table',
 )
 
 
 from .base import Base
+from .order import Order
+from .order_product_association import order_product_association_table
 from .product import Product
 from .profile import Profile
 from .user import User
+
 
 
