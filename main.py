ee4f5cde8f6c2a0e199e5a6aa7cb778bdbe739ce
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-27 13:55:40 +0500

    ad some updates

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-from api_v1 import router as router_v1, fastapi_users
+from api_v1 import router as router_v1
 from core.config import settings
 
-from fastapi import Request, Depends
+from fastapi import Request
 from fastapi import FastAPI
 
 import uvicorn
 
-from core.models import User
+
 
 app = FastAPI()
+
+
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
 @app.get("/")
 def hello_world(request: Request):
     return {"message": "hello world"}
-
-
-current_user = fastapi_users.current_user()
-
-
-@app.get("/protected-route")
-def protected_route(user: User = Depends(current_user)):
-    return f"Hello, {user.username}"
-
-
-@app.get("/unprotected-route")
-def unprotected_route():
-    return f"Hello, anonym"
 
 
 if __name__ == "__main__":
