0aa7005a24a7c986b191043c3a2da3526e156c21
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-27 13:55:30 +0500

    ad relationship to order_product_association_table

--- core/models/product.py
+++ core/models/product.py
@@ -1,6 +1,12 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
+from typing_extensions import TYPE_CHECKING
 
+from core.models import order_product_association_table
 from core.models.base import Base
+
+
+if TYPE_CHECKING:
+    from .order import Order
 
 
 class Product(Base):
@@ -9,3 +15,7 @@ class Product(Base):
     description: Mapped[str]
     count: Mapped[int]
     price: Mapped[int]
+    orders: Mapped[list['Order']] = relationship(
+        secondary=order_product_association_table,
+        back_populates='products',
+    )
