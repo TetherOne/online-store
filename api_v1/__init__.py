f131d2ef5a8b943213e1ed4a8dacca1bddd9f421
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-27 18:02:39 +0500

    update imports

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,13 +1,23 @@
-from fastapi import APIRouter
-from fastapi_users import FastAPIUsers
-from core.models import User
-from api_v1.users.views import user_router
-from api_v1.users.auth import auth_backend
-from api_v1.users.manager import get_user_manager
-from api_v1.users.schemas import UserRead, UserCreate
 from .products.views import router as products_router
 
+from api_v1.users.manager import get_user_manager
+
+from api_v1.users.schemas import UserCreate
+from api_v1.users.schemas import UserRead
+
+from api_v1.users.auth import auth_backend
+
+from api_v1.users.views import user_router
+
+from fastapi_users import FastAPIUsers
+
+from fastapi import APIRouter
+
+from core.models import User
+
+
 router = APIRouter()
+
 
 router.include_router(router=products_router, prefix='/products')
 router.include_router(router=user_router, prefix='/users')
