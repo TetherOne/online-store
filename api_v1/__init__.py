a2234ddddb774dc4b4f9670f5682e53d38832016
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-28 21:57:17 +0500

    update router name

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -7,8 +7,8 @@ from api_v1.users.schemas import UserRead
 
 from api_v1.users.auth import auth_backend
 
-from api_v1.users.views import user_router
-
+from api_v1.users.views import router as users_router
+from api_v1.profiles.views import router as profiles_router
 from fastapi_users import FastAPIUsers
 
 from fastapi import APIRouter
@@ -20,7 +20,8 @@ router = APIRouter()
 
 
 router.include_router(router=products_router, prefix='/products')
-router.include_router(router=user_router, prefix='/users')
+router.include_router(router=users_router, prefix='/users')
+router.include_router(router=profiles_router, prefix='/profiles')
 
 
 fastapi_users = FastAPIUsers[User, int](
