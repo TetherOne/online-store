54d0d99f4f7039e4d6e325ef4dea5c88879b673a
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-29 00:08:55 +0500

    delete default router

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fastapi.testclient import TestClient
+
 from api_v1 import router as router_v1
 
 from core.config import settings
@@ -10,14 +12,9 @@ import uvicorn
 
 
 app = FastAPI()
-
+client = TestClient(app)
 
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
-
-
-@app.get("/")
-def hello_world(request: Request):
-    return {"message": "hello world"}
 
 
 if __name__ == "__main__":
