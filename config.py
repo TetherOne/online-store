996b010553660efb82ab51a582258298db890d33
Pavel Ryaguzov <pashajobber@gmail.com>
2024-02-29 15:52:23 +0500

    ad redis settings

--- config.py
+++ config.py
@@ -7,8 +7,12 @@ load_dotenv()
 
 SECRET_KEY_AUTH = os.environ.get('SECRET_KEY_AUTH')
 SECRET_KEY_MANAGER = os.environ.get('SECRET-KEY-MANAGER')
+
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get('DB_PASS')
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
